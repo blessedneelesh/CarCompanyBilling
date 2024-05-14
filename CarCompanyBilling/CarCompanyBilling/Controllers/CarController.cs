fa57d703390503417f65d573ae675a3cdbd5d51d
Neelesh Maharjan <neeleshmrz@gmail.com>
2024-05-13 22:49:37 -0400

    add HTTP get action controller

---
+++ CarCompanyBilling/CarCompanyBilling/Controllers/CarController.cs
@@ -0,0 +1,40 @@
+﻿using CarCompanyBilling.Models.Doa;
+using CarCompanyBilling.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarCompanyBilling.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly CarDoa _carDoa;
+        public CarController(CarDoa carDoa)
+        {
+            _carDoa = carDoa;
+        }
+
+        [HttpGet]
+        public ActionResult<List<CarDTO>> GetCar() //ActionResult gives flexibility to return NotFound()
+        {
+            var car = _carDoa.GetAllCar();
+            return car;
+        }
+
+        [HttpGet]
+        public ActionResult<List<CarDTO>> GetCarNeverSold() //ActionResult gives flexibility to return NotFound()
+        {
+            var car = _carDoa.GetAllCarNeverSold();
+            return car;
+        }
+
+        [HttpGet]
+        public ActionResult<List<CarInventoryDTO>> GetCarInventory() //ActionResult gives flexibility to return NotFound()
+        {
+            var car = _carDoa.GetAllCarInventory();
+            return car;
+        }
+        
+    }
+}
---
+++ CarCompanyBilling/CarCompanyBilling/Models/Dto/CarDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace CarCompanyBilling.Models.Dto
+{
+    public class CarDTO
+    {
+        public decimal CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public decimal Height { get; set; }
+        public string CarForSale { get; set; }
+        public decimal CarLength { get; set; }
+        public decimal Width { get; set; }
+        public decimal EngineCc { get; set; }
+    }
+}
