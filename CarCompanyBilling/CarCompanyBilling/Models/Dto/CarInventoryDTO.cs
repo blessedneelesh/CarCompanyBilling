6de09f7fd3ca3238c9d350123f5d1487131d1a2a
Neelesh Maharjan <neeleshmrz@gmail.com>
2024-05-23 17:17:46 -0400

    yolo

---
+++ CarCompanyBilling/CarCompanyBilling/Models/Dto/CarInventoryDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CarCompanyBilling.Models.Dto
+{
+    public class CarInventoryDTO
+    {
+        public decimal CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string CarForSale { get; set; }
+        public string WarehouseName { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ CarCompanyBilling/CarCompanyBilling/Models/Dto/CustomerOrdersWithProductsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CarCompanyBilling.Models.Dto
+{
+    public class CustomerOrdersWithProductsDTO
+    {
+        public int CustomerId { get; set; }
+        public string CustomerFullName { get; set; } // firstname + lastname
+        public decimal CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int OrderNum { get; set; } //invoice number
+    }
+}
